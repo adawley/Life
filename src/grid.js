7c676e7847bb816b73bfe3927cd300b5828c3542
Rob Dawley <adawley@gmail.com>
2015-05-08 21:52:21 -0400

    started implementing canvas grid

--- src/grid.js
+++ src/grid.js
@@ -6,20 +6,31 @@
 
         this.elBody = document.getElementsByTagName('body')[0];
         this.elMaster = document.createElement('div');
+        this.elTable = document.createElement('table');
+        this.elCanvas = document.createElement('canvas');
+        this.elCanvas.width = 501;
+        this.elCanvas.height = 381;
+
+        this.ctx = this.elCanvas.getContext("2d");
 
         this.elMaster.setAttribute('class', 'grid');
 
         this.elBody.insertBefore(this.elMaster, this.elBody.firstChild);
+        // this.elMaster.appendChild(this.elTable);
+        this.elMaster.appendChild(this.elCanvas);
     };
 
-    Grid.prototype.show = function(grid) {
+    Grid.prototype.showTable = function(grid) {
 
-        var elCell, elRow, elTable = document.createElement('table'),
+        var elTable = this.elTable,
+            elCell, elRow,
             cellVal,
             numRows = grid.length,
             numCols = grid[0].length;
 
-        this.elCurrentTable = elTable;
+        while (elTable.firstChild) {
+            elTable.removeChild(elTable.firstChild);
+        }
 
         for (var row = 0; row < numRows; row++) {
 
@@ -30,7 +41,7 @@
 
                 elCell = document.createElement('td');
 
-                if(cellVal === 1){
+                if (cellVal === 1) {
                     elCell.setAttribute('class', 'bg-black');
                 }
 
@@ -41,14 +52,54 @@
 
             elTable.appendChild(elRow);
         }
+    };
 
-        while (this.elMaster.firstChild) {
-          this.elMaster.removeChild(this.elMaster.firstChild);
+    Grid.prototype.showCanvas = function(grid) {
+        var ctx = this.ctx,
+            cellWidth = 10,
+            cellHeight = 10,
+            numRows = grid.length,
+            numCols = grid[0].length,
+            gridWidth = numCols * cellWidth,
+            gridHeight = numRows * cellHeight,
+            cellVal;
+
+        // vertical lines
+        for (var x = 0.5; x < gridWidth; x += cellWidth) {
+            ctx.moveTo(x, 0);
+            ctx.lineTo(x, gridHeight);
         }
 
-        this.elMaster.appendChild(elTable);
+        // horizontal lines
+        for (var y = 0.5; y < gridHeight; y += cellHeight) {
+            ctx.moveTo(0, y);
+            ctx.lineTo(gridWidth-1, y);
+        }
 
+        // points
+        for (var row = 0; row < numRows; row++) {
 
+            for (var col = 0; col < numCols; col++) {
+                cellVal = grid[row][col];
+
+                if (cellVal === 1) {
+                    ctx.fillRect(col*cellWidth, row*cellHeight, 10, 10);
+                } else {
+                    ctx.clearRect(col*cellWidth, row*cellHeight, 10, 10);
+                }
+
+            }
+
+        }
+        
+
+        ctx.strokeStyle = "#ddd";
+        ctx.stroke();
+    };
+
+    Grid.prototype.show = function(grid) {
+        // this.showTable(grid);
+        this.showCanvas(grid);
     };
 
     window.Grid = new Grid();
